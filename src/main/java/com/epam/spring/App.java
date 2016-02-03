c2a268c28d7eaafc19e6a8c4c820c1e5d9c5feca
Nazar Lelyak <nazar_lelyak@epam.com>
2016-02-03 18:13:45 +0200

    use spring for test

--- src/main/java/com/epam/spring/App.java
+++ src/main/java/com/epam/spring/App.java
@@ -1,9 +1,17 @@
 package com.epam.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
 
     private Client client;
-    private ConsoleEventLogger eventLogger;
+    private EventLogger eventLogger;
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
 
     private void logEvent(String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
@@ -11,11 +19,12 @@ public class App {
     }
 
     public static void main(String[] args) {
-        App app = new App();
+//        App app = new App(new Client("1", "John Smith"), new ConsoleEventLogger());
+//        app.client = new Client("1", "John Smith");
+//        app.eventLogger = new ConsoleEventLogger();
 
-        app.client = new Client("1", "John Smith");
-        app.eventLogger = new ConsoleEventLogger();
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        App app = context.getBean(App.class);
         app.logEvent("Some event for user 1");
 
     }
--- src/main/java/com/epam/spring/ConsoleEventLogger.java
+++ src/main/java/com/epam/spring/ConsoleEventLogger.java
@@ -1,6 +1,7 @@
 package com.epam.spring;
 
-public class ConsoleEventLogger {
+public class ConsoleEventLogger implements EventLogger {
+    @Override
     public void logEvent(String msg) {
         System.out.println(msg);
     }
